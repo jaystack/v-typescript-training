ee122d6830e550e91659b599e0f1ac7b4ecb3753
pz-gs <peter.zentai@guidesmiths.com>
2022-05-17 13:39:04 +0100

    refine code to use destructuring

--- src/index.ts
+++ src/index.ts
@@ -24,57 +24,48 @@ app.get('/asset-types', (req, res) => {
     });
 });
 
-function createAssetType(assetId: string, assetLabel: string): AssetType {
-    if (!assetId) throw new Error('invalid assetId');
-    if (!assetLabel) throw new Error('invalid assetLabel');
+function createAssetType(id: string, label: string): AssetType {
+    if (!id) throw new Error('invalid assetId');
+    if (!label) throw new Error('invalid assetLabel');
     return {
-        id: assetId,
-        label: assetLabel,
+        id,
+        label,
     }
 }
 
 app.post('/asset-types', (req, res) => {
     const payload: AssetType = req.body;
-    const id = payload.id;
-    const label = payload.label;
+    const { id, label } = payload;
     if (assetTypes.find(assetType => assetType.id === id)) {
         throw new Error('id is not unique')
     };
-    const newAssetType = createAssetType(id, label);
-    assetTypes.push(newAssetType);
-    //assetTypes = [...assetTypes, newAssetType];
+
+    assetTypes = [...assetTypes, createAssetType(id, label)];
     res.status(202).send();
 })
 
 app.put('/asset-types', (req, res) => {
     const payload: AssetType = req.body;
-    const id = payload.id;
-    const label = payload.label;
+    const { id, label } = payload;
 
     if (!assetTypes.find(assetType => assetType.id === id)) {
         throw new Error('id not found')
     };
-    const updatedAssetType = createAssetType(id, label);
-    
-    assetTypes = assetTypes.map(assetType => {
-        return assetType.id === id ? updatedAssetType : assetType
-    })
+
+    assetTypes = assetTypes.map(assetType =>  assetType.id === id ? createAssetType(id, label) : assetType)
     return res.status(202).send();
 })
 
 app.post('/asset-types/:id', (req, res) => {
     const payload: AssetType = req.body;
     const id = req.params.id;
-    const label = payload.label;
+    const { label } = payload;
 
     if (!assetTypes.find(assetType => assetType.id === id)) {
         throw new Error('id not found')
     };
-    const updatedAssetType = createAssetType(id, label);
-    
-    assetTypes = assetTypes.map(assetType => {
-        return assetType.id === id ? updatedAssetType : assetType
-    })
+
+    assetTypes = assetTypes.map(assetType => assetType.id === id ? createAssetType(id, label) : assetType)
     return res.status(202).send();
 })
 
